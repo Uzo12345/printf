d4393cf80847e4846f7529e9a2059a2122ee7504
Uzom12345 <idumauzoma@yahoo.com>
2023-04-25 06:53:46 -0700

    getchars_precision.c

---
+++ getchars_precision.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * getchars_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @checklist: list of arguments.
+ *
+ * Return: accuracy.
+ */
+int getchars_precision(const char *format, int *i, va_list checklist)
+{
+	int curr_i = *i + 1;
+	int accuracy = -1;
+
+	if (format[curr_i] != '.')
+		return (accuracy);
+
+	accuracy = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+		
+			accuracy *= 10;
+			accuracy += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			accuracy = va_arg(checklist, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (accuracy);
+}
