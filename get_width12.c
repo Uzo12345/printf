feacf3e3931e8e5adfab2e0e48aa31956e2fa52d
Uzom12345 <idumauzoma@yahoo.com>
2023-04-25 13:44:45 -0700

    main.h

--- get_width12.c
+++ get_width12.c
@@ -4,26 +4,26 @@
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
- * @checklist: list of arguments.
+ * @list: list of arguments.
  *
- * Return: breadth.
+ * Return: width.
  */
-int get_width(const char *format, int *i, va_list checklist)
+int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
-	int breadth = 0;
+	int width = 0;
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
-			breadth *= 10;
-			breadth += format[curr_i] - '0';
+			width *= 10;
+			width += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			breadth = va_arg(list, int);
+			width = va_arg(list, int);
 			break;
 		}
 		else
@@ -32,5 +32,5 @@ int get_width(const char *format, int *i, va_list checklist)
 
 	*i = curr_i - 1;
 
-	return (breadth);
-i}
+	return (width);
+}
--- getchars_precision.c
+++ getchars_precision.c
@@ -4,32 +4,31 @@
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @checklist: list of arguments.
+ * @list: list of arguments.
  *
- * Return:precision.
+ * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list checklist)
+int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int accuracy = -1;
+	int precision = -1;
 
 	if (format[curr_i] != '.')
-		return (accuracy);
+		return (precision);
 
-	accuracy = 0;
+	precision = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
-		
-			accuracy *= 10;
-			accuracy += format[curr_i] - '0';
+			precision *= 10;
+			precision += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			accuracy = va_arg(checklist, int);
+			precision = va_arg(list, int);
 			break;
 		}
 		else
@@ -38,5 +37,5 @@ int get_precision(const char *format, int *i, va_list checklist)
 
 	*i = curr_i - 1;
 
-	return (accuracy);
+	return (precision);
 }
