2e580adad75c4e7fbf0483dba93f59f321798153
Uzom12345 <idumauzoma@yahoo.com>
2023-04-26 08:15:23 -0700

    getchars_flags.c

--- getchars_flags.c
+++ getchars_flags.c
@@ -6,21 +6,21 @@
  * @i: take a parameter.
  * Return: Flag:
  */
-int get_flags(const char *format, int *i)
+int get_flags(const char *fmt, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
 	int k, curr_i;
 	int flag = 0;
 	const char FLAG_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAG_ARRANGE[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const int FLAG_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; fmt[curr_i] != '\0'; curr_i++)
 	{
 		for (k = 0; FLAG_CH[k] != '\0'; k++)
 			if (fmt[curr_i] == FLAG_CH[k])
 			{
-				flag |= FLAG_ARRANGE[k];
+				flag |= FLAG_ARR[k];
 				break;
 			}
 
